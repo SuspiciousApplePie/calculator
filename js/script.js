7553a634238601767256ceb2ef3af1f605100a52
Shishi <andreiusita6@gmail.com>
2025-08-15 09:39:15 +0800

    test: Add subtraction() with log

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ function addition (num1, num2) {
     return num1 + num2;
 }
 
+function subtraction(num1, num2) {
+    console.log(num1 - num2);
+    return num1 - num2;
+}
+
 function operation (num1, num2, operation) {
     if (operation === '+') {
         addition(num1, num2);
