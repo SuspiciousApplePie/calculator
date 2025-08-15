f65fecee7b2190f25b761c30768e511c1fd5d408
Shishi <andreiusita6@gmail.com>
2025-08-15 09:44:27 +0800

    test: Add multiplication() with logs

--- js/script.js
+++ js/script.js
@@ -8,6 +8,11 @@ function subtraction(num1, num2) {
     return num1 - num2;
 }
 
+function multiplication(num1, num2) {
+    console.log(num1 * num2);
+    return num1 * num2;
+}
+
 function operation (num1, num2, operation) {
     if (operation === '+') {
         addition(num1, num2);
