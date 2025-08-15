0efed285a3654b13daf2c67156c8d23fb5dbf53a
Shishi <andreiusita6@gmail.com>
2025-08-15 09:37:10 +0800

    feat: Add addition checker in operation()

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@ function addition (num1, num2) {
     console.log(num1 + num2);
     return num1 + num2;
 }
+
+function operation (num1, num2, operation) {
+    if (operation === '+') {
+        addition(num1, num2);
+    }
+}
