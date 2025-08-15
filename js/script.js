9e004de8e7cf7a40557c6666999575b1e7d8f6bb
Shishi <andreiusita6@gmail.com>
2025-08-15 09:52:00 +0800

    test: Add division() with log

--- js/script.js
+++ js/script.js
@@ -13,6 +13,11 @@ function multiplication(num1, num2) {
     return num1 * num2;
 }
 
+function division(num1, num2) {
+    console.log(num1 / num2);
+    return num1 / num2;
+}
+
 function operation (num1, num2, operation) {
     if (operation === '+') {
         addition(num1, num2);
